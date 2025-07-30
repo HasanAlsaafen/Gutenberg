1bf46b5b47a943633a239a662264776861c322ae
karmel jaradat <karmeljaradat2005@gmail.com>
2025-07-31 02:48:14 +0300

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">
+      </main>
+      <Footer />
+    </div>
+
+
+  );
+}
+
+export default App;
