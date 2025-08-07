74314725787a0c7197e250ce331fb7d9ae648ded
HasanAlsaafen <hasansaafen1234@gmail.com>
2025-08-07 19:18:24 +0300

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from "./Pages/LandingPage";
 import ServicesPage from "./Pages/ServicesPage";
 import CareerPage from "./Pages/CareerPage";
 import LoginPage from "./Pages/LoginPage";
+import SolutionsPage from "./Pages/SolutionsPage";
 import Dashboard from "./Pages/Dashboard";
 import { Route, Routes } from "react-router-dom";
 import NotFound from "./components/NotFound";
@@ -17,8 +18,10 @@ function App() {
         <Route path="/careers/*" element={<CareerPage />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/" element={<LandingPage />} />
-        <Route path="/solutions" element={<LandingPage />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="/solutions" element={<SolutionsPage />} />
+
+        {/* Add other routes as needed */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
