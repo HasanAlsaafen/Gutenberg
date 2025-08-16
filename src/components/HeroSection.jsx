1b5c625d338289e767ac59ca8dc9ff377cf81661
barahaw <barahawamdeh2003@gmail.com>
2025-08-16 13:15:26 +0300

    link btn

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const HeroSection = () => {
   const navigate = useNavigate();
   const handleClick = () => {
-    navigate("/#contact");
+    navigate("/contact");
   };
 
   return (
